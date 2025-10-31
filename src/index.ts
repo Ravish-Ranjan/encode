a3810a29a541f313d0c94bf9c22c4d1debc8839a
Ravish Ranjan <122588009+Ravish-Ranjan@users.noreply.github.com.>
2025-10-31 23:52:09 +0530

    HOTFIX : changed public html file path

--- src/index.ts
+++ src/index.ts
@@ -35,7 +35,7 @@ app.use(
 );
 
 app.use(express.static(path.join(__dirname, "../public")));
-app.get("{*splat}", (req, res) => {
+app.get("/", (req, res) => {
 	res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
